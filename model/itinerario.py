8216408c509d22d38305fc41bbfcef65e2624553
Bruno Mafei E. D <brunomafei@gmail.com>
2025-09-26 18:08:31 -0300

    itinerario.py

---
+++ model/itinerario.py
@@ -0,0 +1,31 @@
+
+
+Class Meio_transporte:
+    def __init__(self, tipo, capacidade, empresa_transporte):
+        self.__tipo = tipo
+        self.__capacidade = capacidade
+        self.__empresa = empresa_transporte
+
+        @property
+        def tipo(self):
+            return self.__tipo
+
+        @tipo.setter
+        def tipo(self, tipo):
+            self.__tipo = tipo
+
+        @property
+        def capacidade(self):
+            return self.__capacidade 
+
+        @capacidade.setter
+        def capacidade(self, caapacidade):
+            self.__capacidade = capacidade
+        
+        @property
+        def empresa(self):
+            return self.__empresa
+
+        @empresa.setter
+        def empresa(self, empresa):
+            self.__empresa = empresa
