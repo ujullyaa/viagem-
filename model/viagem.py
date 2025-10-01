ae213c4f7355ae705efa55618472352f96f0218b
Bruno Mafei E. D <brunomafei@gmail.com>
2025-10-01 16:57:11 -0300

    declarando propriedades

--- model/viagem.py
+++ model/viagem.py
@@ -2,4 +2,15 @@
 
 class Viajem:
   
-  def __init__(codigo: str, itinerario: Itinerario, data_partida: str, data_chegada: str, meio: Tipo,):
+  def __init__(codigo: str, itinerario: Itinerario, data_partida: str, data_chegada: str, meio: Meio_transporte, empresa: Empresa_transporte, status: str, preco_base: float, pagamentos: Pagamentos, passageiro: Pessoa):
+    
+    self.__
+    self.__
+    self.__
+    self.__
+    self.__
+    self.__
+    self.__
+    self.__
+    self.__
+
