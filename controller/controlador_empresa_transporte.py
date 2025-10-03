84f2883d5716fdf0783d72676840674feca9ebf6
Bruno Mafei E. D <brunomafei@gmail.com>
2025-10-03 16:55:53 -0300

    dados da empresa
    metodos para guardar os dados da empresa, mostra os dados e atualizar os dados

---
+++ controller/controlador_empresa_transporte.py
@@ -0,0 +1,39 @@
+from empresa_transporte import Empresa_transporte
+from view_empresa_transporte import View_empresa_transporte
+
+class Controlador_Empresa_Transporte:
+
+    
+    def __init__(self, empresa_transporte: Empresa_transporte, view_empresa_transporte: View_empresa_transporte):
+
+        self.__empresa_transporte = empresa_transporte
+        self.__view_empresa_transporte = view_empresa_transporte
+
+    
+    @property
+    def empresa_transporte(self):
+        return self.__empresa_transporte
+    
+    @property
+    def view_empresa_transporte(self):
+        return self.__view_empresa_transporte
+
+    def salvar_configuracao(self, nome, cnpj, telefone):
+
+        self.__empresa_transporte.nome_empresa = nome
+        self.__empresa_transporte.cnpj = cnpj
+        self.__empresa_transporte.telefone = telefone
+
+    def exibir_informacoes(self):
+
+        dados = {
+            "Nome": self.__empresa_transporte.nome_empresa,
+            "CNPJ": self.__empresa_transporte.cnpj,
+            "Telefone": self.__empresa_transporte.telefone
+        }
+        
+    def atualizar_informacoes(self, nome, cnpj, telefone):
+
+        self.__empresa_transporte.nome_empresa = nome
+        self.__empresa_transporte.cnpj = cnpj
+        self.__empresa_transporte.telefone = telefone
