7b299738671c4d17736d138c5cf22d887fceb469
Bruno Mafei E. D <brunomafei@gmail.com>
2025-10-07 09:48:38 -0300

    Update controlador_empresa_transporte.py

--- controller/controlador_empresa_transporte.py
+++ controller/controlador_empresa_transporte.py
@@ -3,25 +3,25 @@ from model.empresa_transporte import Empresa_transporte
 
 class Controlador_Empresa_Transporte:
 
-    def __init__(self, empresa_transportes: list, view_empresa_transporte: View_empresa_transporte):
+    def __init__(self, empresas_transporte: list, tela_empresa_transporte: Tela_empresa_transporte):
 
-        self.__empresa_transporte = empresa_transporte
-        self.__view_empresa_transporte = view_empresa_transporte
+        self.__empresas_transporte = empresa_transporte
+        self.__tela_empresa_transporte = tela_empresa_transporte
 
     @property
     def empresa_transporte(self):
-        return self.__empresa_transporte
+        return self.__empresas_transporte
 
     @property
     def view_empresa_transporte(self):
-        return self.__view_empresa_transporte
+        return self.__Tela_empresa_transporte
 
     def salvar_configuracao(self, nome, cnpj, telefone):
 
         self.__empresa_transporte.nome_empresa = nome
         self.__empresa_transporte.cnpj = cnpj
         self.__empresa_transporte.telefone = telefone
-        self.view_empresa_transporte.exibir_mensagem(
+        self.__tela_empresa_transporte.exibir_mensagem(
             f"Configuração salva para: {nome}")
 
     def exibir_informacoes(self):
@@ -31,5 +31,6 @@ class Controlador_Empresa_Transporte:
             "CNPJ": self.__empresa_transporte.cnpj,
             "Telefone": self.__empresa_transporte.telefone
         }
-        self.view_empresa_transporte.mostrar_detalhes_empresa(dados)
+        self.__tela_empresa_transporte.mostrar_detalhes_empresa(dados)
+
 
