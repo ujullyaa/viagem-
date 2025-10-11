59299493c1b909f1a86beb157719a0dd37c0a466
Bruno Mafei E. D <brunomafei@gmail.com>
2025-10-10 21:36:45 -0300

    controlador e tela empresa transporte

--- controller/controlador_empresa_transporte.py
+++ controller/controlador_empresa_transporte.py
@@ -1,39 +1,51 @@
-from model.empresa_transporte import Empresa_transporte
+from view.tela_empresa_transporte import TelaEmpresaTransporte
+from model.empresa_transporte import EmpresaTransporte
 
 
-class Controlador_Empresa_Transporte:
+class ControladorEmpresaTransporte():
 
-    def __init__(self, empresas_transporte: list, tela_empresa_transporte: Tela_empresa_transporte):
+    def __init__(self, controlador_controladores):
 
         self.__empresas_transporte = []
-        self.__tela_empresa_transporte = tela_empresa_transporte
-
-    @property
-    def empresas_transporte(self):
-        return self.__empresas_transporte
-
-    @property
-    def tela_empresa_transporte(self):
-        return self.__tela_empresa_transporte
-
-    def salvar_configuracao(self, nome, cnpj, telefone):
-
-        self.__empresas_transporte.nome_empresa = nome
-        self.__empresas_transporte.cnpj = cnpj
-        self.__empresas_transporte.telefone = telefone
-        self.__tela_empresa_transporte.exibir_mensagem(
-            f"Configuração salva para: {nome}")
-
-    def exibir_informacoes(self):
-
-        dados = {
-            "Nome": self.__empresas_transporte.nome_empresa,
-            "CNPJ": self.__empresas_transporte.cnpj,
-            "Telefone": self.__empresas_transporte.telefone
-        }
-        self.__tela_empresa_transporte.mostrar_detalhes_empresa(dados)
+        self.__tela_empresa_transporte = TelaEmpresaTransporte()
+        self.__controlador_controladores = controlador_controladores
 
 
+    def incluir_empresa(self):
+        dados_empresa = self.__tela_empresa_transporte.pega_dados_empresa()
+        empresa = EmpresaTransporte(dados_empresa["nome empresa"], dados_empresa["telefone"], dados_empresa["cnpj"])
+        self.__empresas_transporte.append(empresa)
+
+    def lista_empresa(self):
+        for empresa_transporte in self.__empresas_transporte:
+            self.__tela_empresa_transporte.mostra_empresa({"nome": empresa_transporte.nome_empresa, "telefone": empresa_transporte.telefone, "cnpj": empresa_transporte.cnpj})
+
+    def pega_empresa_por_cnpj(self, cnpj: int):
+        for empresa_transporte in self.__empresas_transporte:
+            if(empresa_transporte.cnpj == cnpj):
+                return empresa_transporte
+        return None
+
+    def excluir_empresa(self):
+        self.lista_empresa()
+        cnpj_empresa = self.__tela_empresa_transporte.seleciona_empresa()
+        empresa = self.pega_empresa_por_cnpj(cnpj_empresa)
+
+        if(empresa is not None):
+            self.__empresas_transporte.remove(empresa)
+            self.lista_empresa()
+        else:
+            self.__tela_empresa_transporte.mostra_mensagem("ATENCAO: Empresa não existente")
+
+    def retornar(self):
+        self.__controlador_controladores.abre_tela()
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.incluir_amigo, 2: self.lista_amigos, 3: self.excluir_amigo, 0: self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_empresa_transporte.tela_opcoes()]()
 
 
 
